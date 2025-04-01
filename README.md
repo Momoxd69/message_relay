70394b4600b021550d31c5647c6d8ada7f1c11db
unknown <you@example.com>
2025-04-01 14:22:18 +0530

    first

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # message_relay
 shows what has been given
+this program retuen the value given by the user
+
---
+++ text_prg.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+ {
+    char text[5];
+    scanf("Enter some text: ");
+    printf("You entered: %s", text);
+
+    return 0;
+}
